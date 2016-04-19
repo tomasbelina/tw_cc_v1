97dca491b4195211c70f68436b1791b0a45b5078
Laky <l_a_k_y@centrum.cz>
2016-04-19 21:09:16 +0200

    Initial Commit

---
+++ tw_cc_v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tw_cc_v1
+//
+//  Created by Lukáš Vraný on 19.04.16.
+//  Copyright © 2016 Laky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
